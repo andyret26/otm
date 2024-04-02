e8dbe9847fca18c671859b9c1d0083c372dda202
andyret26 <andyret26@gmail.com>
2024-04-02 11:23:59 +0200

    :construction: Toolip component

---
+++ src/components/general-purpose/ToolTip.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tooltip">
+    <slot></slot>
+    <div class="tooltip__text">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped lang="scss"></style>
