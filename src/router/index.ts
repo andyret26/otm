f5a67b0906cdef56b7c14d17d66208481a3f3879
andyret26 <andyret26@gmail.com>
2024-04-02 12:08:03 +0200

    :sparkles: Added route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuth0 } from '@auth0/auth0-vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/user/:userId/dashboard',
-      name: 'Dashboard',
-      component: () => import('../views/DashboardView.vue')
+      path: '/user/:id',
+      name: 'User',
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: () => import('../views/DashboardView.vue')
+        }
+      ]
     }
   ]
+})
+
+router.beforeEach((to) => {
+  const { isAuthenticated } = useAuth0()
+  if (!isAuthenticated.value && to.name == 'Dashboard') {
+    return '/'
+  }
 })
 
 export default router
