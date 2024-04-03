8c1f47dc6287f046830e16cf6278092adb719447
andyret26 <andyret26@gmail.com>
2024-04-03 10:48:20 +0200

    :sparkles: Created IconBtn component

---
+++ src/components/general-purpose/IconBtn.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    class="btn"
+    :style="{
+      backgroundColor: `var(--osu-${props.color})`,
+      color: props.textColor,
+      '--size': props.size + 'px'
+    }"
+  >
+    <v-icon :name="props.iconName" />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  iconName: string
+  color: 'purple' | 'blue' | 'green' | 'pink' | 'brown' | 'yellow'
+  textColor: string
+  size?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 35
+})
+</script>
+
+<style scoped lang="scss">
+.btn {
+  --size: 35px;
+  font-weight: bolder;
+  font-size: 16px;
+
+  height: var(--size);
+  width: var(--size);
+
+  padding: 5px 10px;
+  border: none;
+  border-radius: 50%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
