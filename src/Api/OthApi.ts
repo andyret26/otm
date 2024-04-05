73670fa87db2b83a7b03dc568f811d2c63b4673b
andyret26 <andyret26@gmail.com>
2024-04-05 14:47:33 +0200

    :sparkles: Added Api function to createTournament

--- src/Api/OthApi.ts
+++ src/Api/OthApi.ts
@@ -1,12 +1,30 @@
-import type { DashboardTournament } from '@/Types'
-import axios from 'axios'
+import type { DashboardTournament, CreateTouernament } from '@/Types'
+import axios, { type AxiosResponse } from 'axios'
 
-const OTH_API = 'https://oth-api.azurewebsites.net/api/v1/otm'
+let OTH_API: string
+// @ts-ignore
+if (process.env.NODE_ENV === 'production') {
+  OTH_API = 'https://oth-api.azurewebsites.net/api/v1/otm'
+  // @ts-ignore
+} else if (process.env.NODE_ENV === 'development') {
+  OTH_API = 'http://localhost:5110/api/v1/otm'
+}
 
 export async function getDashboardData(sub: string): Promise<DashboardTournament[]> {
   const id = subToId(sub)
   const resp = await axios.get<DashboardTournament[]>(`${OTH_API}/host/${id}/tournaments`)
   return resp.data
+}
+
+export async function createTournament(
+  tournament: CreateTouernament,
+  token: string
+): Promise<AxiosResponse> {
+  const resp = await axios.post(`${OTH_API}/tournament`, tournament, {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  console.log(resp)
+  return resp
 }
 
 function subToId(sub: string): number {
