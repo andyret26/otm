5eaed670700da6bcf4bfee4f7e927e4f01a5e335
andyret26 <andyret26@gmail.com>
2024-04-05 14:50:41 +0200

    :sparkles: added types
    Types added: CreatTournament, and ResponseError

--- src/Types.ts
+++ src/Types.ts
@@ -1,5 +1,16 @@
 export type DashboardTournament = {
   id: number
   name: string
-  formuPostLink: string
+  formuPostLink?: string
+}
+
+export type CreateTouernament = {
+  name: string
+  formuPostLink?: string
+}
+
+export type ResponseError = {
+  title: string
+  status: number
+  detail: string
 }
