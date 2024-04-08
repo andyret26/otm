00f5b03180320ee38aa443d07f0144a03340375f
andyret26 <andyret26@gmail.com>
2024-04-08 10:33:02 +0200

    :sparkles: created components: HomeView, TournamentView and tournamentCard

---
+++ src/components/dashboard/TournamentCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <RouterLink :to="`/tournament/${tournament.id}`" class="tournament-card">
+    <p class="tournament-card__name">{{ tournament.name }}</p>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import type { DashboardTournament } from '@/Types'
+import { RouterLink } from 'vue-router'
+
+interface Props {
+  tournament: DashboardTournament
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped lang="scss">
+.tournament-card {
+  background-color: var(--bg3);
+  height: 75px;
+  border-radius: 99999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: var(--bg2);
+  }
+}
+</style>
---
+++ src/views/TournamentView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>a tournament view</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { DashboardTournament } from '@/Types'
+
+interface Props {
+  tournament: DashboardTournament
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped lang="scss"></style>
