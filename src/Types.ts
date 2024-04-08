fd74bf1f2de8ba1f572ab90c22c48881e40893c0
andyret26 <andyret26@gmail.com>
2024-04-08 10:32:01 +0200

    :sparkles: added types for round player and team

--- src/Types.ts
+++ src/Types.ts
@@ -2,6 +2,36 @@ export type DashboardTournament = {
   id: number
   name: string
   formuPostLink?: string
+
+  rounds: Round[]
+  players: Player[]
+  Teams: Team[]
+  Staff: Staff[]
+}
+
+export type Round = {
+  id: number
+  name: string
+}
+
+export type Player = {
+  id: number
+  username: string
+  avatar_url: string
+  global_rank?: number
+  country_code: string
+}
+
+export type Team = {
+  id: number
+  teamName: string
+  players: Player[]
+}
+
+export type Staff = {
+  id: number
+  username: string
+  roles: string[]
 }
 
 export type CreateTouernament = {
