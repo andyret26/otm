1732aab1c240fc2721692a2000f8c8d5bc8f5e67
andyret26 <andyret26@gmail.com>
2024-04-10 09:59:31 +0200

    :wrench: Updated CreatTournament

--- src/components/dashboard/CreateTournament.vue
+++ src/components/dashboard/CreateTournament.vue
@@ -11,8 +11,38 @@
         :icon-size="0.75"
       />
     </div>
-    <InputField :bg-color="'var(--bg3)'" placeholder="Name" v-model="name" />
+    <InputField
+      :bg-color="'var(--bg3)'"
+      placeholder="Name"
+      v-model="name"
+      label-text="Tournament Name"
+    />
     <InputField :bg-color="'var(--bg3)'" placeholder="Forum post link" v-model="forumPostLink" />
+    <RadioGroup
+      :options="radioOptions"
+      :value="selectedOption"
+      id="radio-group-create-tournament"
+      @change="
+        (e) => {
+          selectedOption = e.value
+          formatDisabled = e.value === 'Solo'
+        }
+      "
+    />
+    <SelectBox
+      label="Format"
+      :options="formatOptions"
+      v-model="selectedFormat"
+      :disabled="formatDisabled"
+    />
+    <SelectBox
+      label="Max team size"
+      :options="maxTeamSizeOptions"
+      v-model="seclectedTeamSize"
+      :disabled="formatDisabled"
+    />
+    <InputField :bg-color="'var(--bg3)'" placeholder="Rank range" v-model="rankRange" />
+
     <ButtonComp @click="handleCreateClick" btn-text="Create" color="green" text-color="black" />
   </div>
 </template>
@@ -21,21 +51,45 @@
 import ButtonComp from '../general-purpose/ButtonComp.vue'
 import InputField from '../general-purpose/InputField.vue'
 import IconBtn from '../general-purpose/IconBtn.vue'
+import SelectBox from '../general-purpose/SelectBox.vue'
 import { createTournament } from '@/Api/OthApi'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import type { CreateTouernament, ResponseError } from '@/Types'
 import { useAuth0 } from '@auth0/auth0-vue'
 import { useToast } from 'vue-toastification'
 import { AxiosError } from 'axios'
+import RadioGroup from '../general-purpose/RadioGroup.vue'
 
 const emit = defineEmits(['tournamentCreated', 'closeClicked'])
 const { idTokenClaims } = useAuth0()
 const toast = useToast()
 
+const radioOptions = [
+  { label: 'Teams', value: 'Teams' },
+  { label: 'Solo', value: 'Solo' }
+]
+
+const formatOptions = ['1v1', '2v2', '3v3', '4v4']
+const maxTeamSizeOptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
 const name = ref('')
 const forumPostLink = ref('')
+const selectedOption = ref<string>('Teams')
+const selectedFormat = ref<'1v1' | '2v2' | '3v3' | '4v4'>('1v1')
+const seclectedTeamSize = ref<string>('1')
+const rankRange = ref('')
+const formatDisabled = ref(selectedOption.value === 'Solo')
+
+watch(selectedOption, (newVal) => {
+  if (newVal === 'Solo') {
+    selectedFormat.value = '1v1'
+    seclectedTeamSize.value = '1'
+    formatDisabled.value = true
+  }
+})
 
 const handleCreateClick = async () => {
+  console.log(selectedOption.value)
   const toastId = toast.info('Creating tournament...', {
     hideProgressBar: true,
     timeout: 0
@@ -43,7 +97,11 @@ const handleCreateClick = async () => {
 
   const tournamentToCreate: CreateTouernament = {
     name: name.value,
-    formuPostLink: forumPostLink.value
+    formuPostLink: forumPostLink.value,
+    isTeamTourney: selectedOption.value === 'Teams',
+    format: selectedFormat.value,
+    maxTeamSize: parseInt(seclectedTeamSize.value),
+    rankRange: rankRange.value
   }
 
   const token = idTokenClaims.value!.__raw
@@ -65,7 +123,7 @@ const handleCreateClick = async () => {
 .create-tournament {
   position: absolute;
   width: 300px;
-  height: 260px;
+  height: 500px;
   top: 50%;
   margin-top: -130px;
   background-color: var(--bg2);
