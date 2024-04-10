5e9ad7251475ca033104df076c2f8cc7d25f4978
andyret26 <andyret26@gmail.com>
2024-04-10 09:59:00 +0200

    :sparkles: Created SelectBox component

---
+++ src/components/general-purpose/SelectBox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="select">
+    <label
+      class="select__label"
+      v-if="label"
+      for="select"
+      :style="{ color: disabled ? 'lightgray' : 'white' }"
+      >{{ label }}</label
+    >
+    <select class="select__field" name="select" id="select" v-model="model" :disabled="disabled">
+      <option class="select__option" v-for="option in options" :value="option" :key="option">
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  options: string[]
+  disabled?: boolean
+  label?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  disabled: false
+})
+const model = defineModel()
+</script>
+
+<style scoped lang="scss">
+.select {
+  width: 200px;
+  &__label {
+    font-size: 16px;
+    margin-left: 5px;
+  }
+  &__field {
+    width: 100%;
+    background-color: var(--bg3);
+    color: inherit;
+    border: none;
+    border-radius: var(--base-border-radius);
+    padding: 5px;
+    font-size: 16px;
+    font-family: inherit;
+  }
+}
+</style>
