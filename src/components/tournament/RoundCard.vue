2a68903533bb6352ef3eaf302f97c75c165928f8
andyret26 <andyret26@gmail.com>
2024-04-11 10:01:30 +0200

    :sparkles: Created Components
    TeamCard,RoundCard

---
+++ src/components/tournament/RoundCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="round">round</div>
+</template>
+
+<script setup lang="ts">
+import type { Round } from '@/Types'
+
+interface Props {
+  round: Round
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped></style>
---
+++ src/components/tournament/TeamCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ team.teamName }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Team } from '@/Types'
+
+interface Props {
+  team: Team
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped></style>
