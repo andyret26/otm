f55c1da257032c4d7ba0f02356b5191fd3b0837d
andyret26 <andyret26@gmail.com>
2024-04-16 11:35:21 +0200

    :heavy_minus_sign: Deleted .htaccess updated router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuth0 } from '@auth0/auth0-vue'
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
