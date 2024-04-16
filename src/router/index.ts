8f6623da60fbbe4310d03e152b75361a16b638a2
andyret26 <andyret26@gmail.com>
2024-04-16 11:25:37 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuth0 } from '@auth0/auth0-vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
