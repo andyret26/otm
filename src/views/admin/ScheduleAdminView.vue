e54bfd340b0f3707ca35262855f22234cd991cab
andyret26 <andyret26@gmail.com>
2024-06-06 14:37:59 +0200

    :construction: schedule admin view

--- src/views/admin/ScheduleAdminView.vue
+++ src/views/admin/ScheduleAdminView.vue
@@ -3,29 +3,34 @@
     <IconBtn
       class="stats__admin-btn"
       v-if="isAuthenticated"
-      title="Admin view"
-      @click="handleAdminClick"
-      icon-name="md-adminpanelsettings"
-      color="brown"
-      text-color="black"
+      title="Public view"
+      @click="handlePublicClick"
+      icon-name="fa-globe"
+      color="blue"
+      text-color="white"
       :icon-size="1.2"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { isAuthorized } from '@/Api/OtmApi'
+import { getRound, isAuthorized } from '@/Api/OtmApi'
+import type { Round } from '@/Types'
 import IconBtn from '@/components/common/IconBtn.vue'
 import { useAuth0 } from '@auth0/auth0-vue'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
 
 const { isAuthenticated, idTokenClaims } = useAuth0()
 const route = useRoute()
 const router = useRouter()
+const toast = useToast()
 
 const tourneyId = parseInt(route.path.split('/')[2])
 const roundId = parseInt(route.path.split('/')[4])
+
+const round = ref<Round | null>(null)
 
 onMounted(async () => {
   if (
@@ -33,12 +38,16 @@ onMounted(async () => {
     !(await isAuthorized(idTokenClaims.value!.__raw, tourneyId, ['admin', 'host']))
   ) {
     router.push(`/tournament/${tourneyId}/round/${roundId}/stats`)
+    toast.error('You are not authorized to view this page')
     return
   }
+
+  const resp = await getRound(roundId)
+  round.value = resp.data
 })
 
-const handleAdminClick = () => {
-  router.push(`/tournament/${tourneyId}/round/${roundId}/schedule/admin`)
+const handlePublicClick = () => {
+  router.push(`/tournament/${tourneyId}/round/${roundId}/schedule`)
 }
 </script>
 
